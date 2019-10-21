4a9e6e5bd99ad4874c064a5625e6a10ee2fac682
luigiascione <luigi__96@hotmail.it>
2019-10-21 21:15:21 +0200

    First commit

--- app/src/main/java/com/example/home/MainActivity.java
+++ app/src/main/java/com/example/home/MainActivity.java
@@ -1,17 +1,29 @@
 package com.example.home;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
-
+    static boolean autenticato = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button Blogin= findViewById(R.id.buttonLogin);
+
+        Blogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent Login = new Intent(MainActivity.this,login.class);
+
+                startActivity(Login);             }
+        });
     }
 }

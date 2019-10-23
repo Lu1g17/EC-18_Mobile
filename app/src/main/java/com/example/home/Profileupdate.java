b5f3d0e081069ad499248dc7c80db45105f0128d
luigiascione <luigi__96@hotmail.it>
2019-10-23 15:41:49 +0200

    21 ottobre 22:24

---
+++ app/src/main/java/com/example/home/Profileupdate.java
@@ -0,0 +1,17 @@
+package com.example.home;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Profileupdate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profileupdate);
+
+
+
+    }
+}

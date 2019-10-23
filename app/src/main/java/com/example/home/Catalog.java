c3b647ec2f00bbd52c6347727e08b5b8d38ef902
luigiascione <luigi__96@hotmail.it>
2019-10-23 16:41:44 +0200

    21 ottobre 22:24

--- app/src/main/java/com/example/home/Catalog.java
+++ app/src/main/java/com/example/home/Catalog.java
@@ -1,6 +1,10 @@
 package com.example.home;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +14,36 @@ public class Catalog extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.catalog);
+
+        TextView abbigliamento = findViewById(R.id.textViewabbigliamento);
+
+        abbigliamento.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                System.out.println("hai premuto su abbigliamento");
+            }
+
+        });
+
+        TextView calzature = findViewById(R.id.textViewcalzature);
+
+        abbigliamento.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                System.out.println("hai premuto su calzature");
+            }
+
+        });
+
+        TextView accessori = findViewById(R.id.textViewaccessori);
+
+        abbigliamento.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                System.out.println("hai premuto su accessori");
+            }
+
+        });
 
     }
 }

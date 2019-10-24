4221ed038a06aa3d2ac619db24e6ee8413c7ddc7
luigiascione <luigi__96@hotmail.it>
2019-10-24 19:02:23 +0200

    23 ottobre 23:15

---
+++ app/src/main/java/com/example/home/Paypal.java
@@ -0,0 +1,8 @@
+package com.example.home;
+
+public class Paypal {
+
+    public static final String PAYPAL_CLIENT_ID = "AXOdllnlprMKQb_AKbS955Yh6UCnaUfhWbIKdekrxNclQJzOdEwhfbg0iCVP5nkCEbmByyuNFUNe9SCx";
+
+
+}
